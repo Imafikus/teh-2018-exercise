3af2f2ca5a56ba4261869f1cbd8e409c974b3683
Aleksa Tešić <aleksatesicteske@gmail.com>
2018-05-18 18:39:37 +0200

    Add air resistance

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import math
 
 
 def calc_curr_vx(vx):
-    vx = vx
+    vx -= vx**2 * CONST_DELTA_T * CONST_K
     return vx
 
 def calc_curr_vy(vy):
-    vy -= CONST_G * t_curr**2 / 2
+    sgn_y = abs(vy) / vy
+    vy -= vy**2 * CONST_DELTA_T * CONST_K * sgn_y
+    vy -= CONST_G * t_curr
     return vy
-
 
 #constants
 CONST_G = 9.81
-CONST_STEP = 0.1
+CONST_DELTA_T = 0.1
+CONST_K = 0.01
 
 #input values
 v_start = float(input()) 
@@ -36,16 +38,16 @@ y_coords = []
 
 while(t_curr <= float(num_secs)):
     x_coords.append(x)
-    
     y_coords.append(y)
 
-    x += vx_curr * t_curr
-    y += vy_curr * t_curr
-
+    x += vx_curr * CONST_DELTA_T
+    y += vy_curr * CONST_DELTA_T
+  
     vx_curr = calc_curr_vx(vx_curr)
+    print(vy_curr)
     vy_curr = calc_curr_vy(vy_curr)
 
-    t_curr += CONST_STEP
+    t_curr += CONST_DELTA_T
 
 plt.plot(x_coords, y_coords, 'r')
 plt.show()
